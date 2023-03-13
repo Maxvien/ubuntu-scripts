b81567e94826f1d55f855229756637184654ef2d
Vien Dinh <maxvien@gmail.com>
2023-03-13 17:27:51 +0700

    disable magnus-autostart

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,4 +3,6 @@ sudo apt update
 sudo apt install i3 --no-install-recommends 
 sudo apt-get install rofi polybar
 
+sudo mv /etc/xdg/autostart/magnus-autostart.desktop /etc/xdg/autostart/magnus-autostart.desktop.backup
+
 ./setup/sync.sh
