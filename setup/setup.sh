9a064ab0ee54a4a44e90c6a0d9521b446a567673
Vien Dinh <maxvien@gmail.com>
2023-03-14 12:55:58 +0700

    config i3wm with ubuntu mate

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,11 @@
+# install i3wm
 sudo apt update
-
 sudo apt install i3 --no-install-recommends
 
+# config ubuntu mate with i3wm
+gsettings set org.mate.session.required-components windowmanager "i3"
+gsettings set org.mate.session required-components-list "['windowmanager', 'panel']"
+gsettings set com.solus-project.brisk-menu hot-key ""
+
+# sync config files
 ./setup/sync.sh
